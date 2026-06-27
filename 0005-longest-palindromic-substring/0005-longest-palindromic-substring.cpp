0277ce9bc8cb0cb77f158eee67f84a2f2912d9f0
Moufee Al Doushari <doushari.dipto212@gmail.com>
2026-06-27 13:15:01 +0600

    Time: 7 ms (86.68%), Space: 9.3 MB (79.85%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  int expand(string &s ,int left,int right)
+  {
+    while(left>=0&&right<s.size()&&s[left]==s[right])
+    {
+        left--;
+        right++;       
+    }
+    return right-left-1;
+  }
+    string longestPalindrome(string s) {
+       int left=0,right=0;
+       for(int i=0;i<s.size();i++)
+       {
+        int len1=expand(s,i,i);
+        int len2=expand(s,i,i+1);
+        int len=max(len1,len2);
+        if(len>right-left+1)
+        {           
+            left=i-(len-1)/2;
+            right=i+(len)/2;
+        }
+       }
+       return s.substr(left,right-left+1);
+    }
+};
